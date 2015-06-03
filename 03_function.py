c5e9e95e6e859777900d2cb157e92aba7c7b48bd
sdlee <mecataumk2@gmail.com>
2015-06-03 19:09:02 +0900

    define add function

--- 03_function.py
+++ 03_function.py
@@ -1,5 +1,4 @@
 def print_me( s ):
-    "This prints a passed string into this function."
     print s
     return
 
@@ -8,6 +7,5 @@ def add(arg1, arg2):
     return sum
 
 print "print_me"
-
 print_me("call print_me")
 print add(10, 20)
