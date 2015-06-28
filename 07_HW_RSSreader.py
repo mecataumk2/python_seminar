09503d306aeacff0b6b3b1fc00713914b6089571
sdlee <mecataumk2@gmail.com>
2015-06-28 21:53:50 +0900

    Python seminar 6th. Make RSS Reader(HW). Hash and function update

--- 07_HW_RSSreader.py
+++ 07_HW_RSSreader.py
@@ -28,39 +28,33 @@ def print_feed_title(rss_filename):
     for line in lines:
         m = re_title.search(line)
         if m:
-            print"Title: ", (m.group(2))
+            print (m.group(2))
 
 def remove_taglines(filename, tagname):
-    file = open(filename, 'r')
+    f = open(filename, 'r')
     filename_tmp = filename + '_tmp'
-    file_tmp = open(filename_tmp, 'w')
-    lines_file = file.readlines()
-
+    f_tmp = open(filename_tmp, 'w')
     re_pubDate = re.compile(tagname)
 
-    for line in lines_file:
-        m = re_pubDate.search(line)
-        if m == None:
-        # if not m:
-            file_tmp.write(line)
-    file.close()
-    file_tmp.close()
+    lines = f.readlines()
+    for line in lines:
+        re_object = re_pubDate.search(line)
+        if not re_object:
+            f_tmp.write(line)
+    f.close()
+    f_tmp.close()
     return filename_tmp
 
 def rss_reader(rss_url, filename):
-    # print filename_tmp                                  #testcode,testcode
-    # if os.path.isfile("darkan84.xml") == False:
 
-
-
-    if not os.path.isfile(filename):                                 # when downloaded file is not exist
+    if not os.path.exists(filename):                                 # when downloaded file is not exist
         urllib.urlretrieve (rss_url, filename)
         print "New RSS Feed is created succesfully."
         print_feed_title(filename)
         return
 
-    if os.path.isfile("tmp.xml"):                                   # when tempolory file is exist (pre check. Is this not mandatory?.I need some test.)
-        os.remove("tmp.xml")
+    # if os.path.isfile("tmp.xml"):                                   ##### when tempolory file is exist (pre check. Is this not mandatory?.I need some test.)
+    #     os.remove("tmp.xml")
 
     urllib.urlretrieve (rss_url, "tmp.xml")
 
@@ -91,7 +85,7 @@ def rss_reader(rss_url, filename):
     # after_file_tmp.close()
 
     if not filecmp.cmp(no_pubDate_filename, no_pubDate_tmp):
-        print "Update!"                                             # move tmp.xml to filename
+        print "Update!\n"                                             # move tmp.xml to filename.   ##### Move function is exist in library(os)?
         downloaded_file = open(filename, 'w')
         new_file = open("tmp.xml", 'r')
         lines = new_file.readlines()
@@ -101,7 +95,7 @@ def rss_reader(rss_url, filename):
         downloaded_file.close()
         new_file.close()
     else:
-        print "Update is not available"
+        print "Update is not available.\n"
 
     os.remove("tmp.xml")
     os.remove(no_pubDate_filename)
@@ -109,9 +103,8 @@ def rss_reader(rss_url, filename):
 
     print_feed_title(filename)
 
-# rss_reader("http://onenable.tumblr.com/rss")
-# rss_reader("http://blog.rss.naver.com/darkan84.xml")
 # url = raw_input('Enter RSS Feed url : ')
+# url = "http://onenable.tumblr.com/rss"
 url = "http://blog.rss.naver.com/darkan84.xml"
 filename = string_to_md5(url)
-rss_reader(url, filename)
+rss_reader(url, filename)                                       ##### rss_reader
